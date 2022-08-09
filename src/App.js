ec1c5e7bacb054e1c9d7c80b8aebf1c9eb4b7bef
jass <maanjaswinder2412@gmail.com>
2022-08-09 01:47:35 -0400

    First deployment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import LoadingBar from 'react-top-loading-bar'
+
+export default class App extends Component {
+
+
+
+  render() {
+    return (
+      <div>
+          <LoadingBar
+        color='blue'
+        progress='100'
+        height='5px'
+        loaderSpeed='1000'
+        waitingTime='1000'
+        transitionTime='1000'
+        
+      />
+        <Navbar/>
+        <Home/>
+      </div>
+    );
+  }
 }
-
-export default App;
